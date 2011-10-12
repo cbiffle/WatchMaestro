67e772d384ce453683221461e9a6bae03193ac1e
Cliff L. Biffle <cbiffle@gmail.com>
2011-10-11 20:11:55 -0700

    Implemented message encapsulation.

--- src/com/cliffle/watch/io/WatchMessageStream.java
+++ src/com/cliffle/watch/io/WatchMessageStream.java
@@ -1,5 +1,9 @@
 package com.cliffle.watch.io;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 
 /**
  * Provides MetaWatch-compatible message encapsulation and integrity checking
@@ -8,6 +12,11 @@ package com.cliffle.watch.io;
  * @author cbiffle
  */
 public class WatchMessageStream {
+  // The encapsulation method restricts messages to 255 bytes.
+  private static final int MAX_MESSAGE_SIZE = 255;
+  // Messages are headed by an ASCII Start-Of-Frame character.
+  static final int START_OF_FRAME = 0x01;
+  
   public static short computeCrc(WatchMessage message) {
     short crc = (short) 0xFFFF;
     crc = crcStep(crc, message.getType());
@@ -28,6 +37,32 @@ public class WatchMessageStream {
       if (c15 ^ bit) crc ^= 0x1021;  // 0001 0000 0010 0001 (0, 5, 12)
     }
     return crc;
+  }
+  
+  // Visible for testing.
+  static int lengthOnWire(WatchMessage message) {
+    // Start-of-Frame, Length, Type, Payload, CRCx2.
+    return 1 + 1 + 1 + message.getPayload().length + 2;
+  }
+  
+  private final BufferedOutputStream out;
+
+  public WatchMessageStream(OutputStream out) {
+    this.out = new BufferedOutputStream(out, MAX_MESSAGE_SIZE);
+  }
+  
+  public void write(WatchMessage message) throws IOException {
+    out.write(START_OF_FRAME);
+    out.write(lengthOnWire(message));
+    out.write(message.getType());
+    out.write(message.getPayload());
+    
+    // CRCs are sent little-endian.
+    short crc = computeCrc(message);
+    out.write(crc);
+    out.write(crc >> 8);
+    
+    out.flush();
   }
   
 }
--- test/com/cliffle/watch/io/WatchMessageStreamTest.java
+++ test/com/cliffle/watch/io/WatchMessageStreamTest.java
@@ -2,6 +2,9 @@ package com.cliffle.watch.io;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import org.junit.Test;
 
 import com.cliffle.watch.io.WatchMessage;
@@ -14,5 +17,50 @@ public class WatchMessageStreamTest {
     
     final short expectedCrc = 0x29AA;  // Computed using reference impl
     assertEquals(expectedCrc, WatchMessageStream.computeCrc(input));
+  }
+  
+  @Test public void testLengthOnWire() {
+    // Too simple to fail?  Maybe.  But I'm feeling paranoid.
+    WatchMessage input = new WatchMessage((byte) 0xA5,
+        new byte[] { 0x01, (byte) 0xDE, 0x55 });
+    assertEquals(1 + 1 + 1 + 3 + 2, WatchMessageStream.lengthOnWire(input));
+  }
+  
+  @Test public void testWriteEmpty() throws IOException {
+    WatchMessage empty = new WatchMessage((byte) 0x42, new byte[0]);
+    ByteArrayOutputStream sink = new ByteArrayOutputStream();
+    WatchMessageStream stream = new WatchMessageStream(sink);
+    
+    stream.write(empty);
+    
+    byte[] result = sink.toByteArray();
+    assertEquals(1 + 1 + 1 + 0 + 2, result.length);
+    assertArrayEquals(new byte[] {
+        WatchMessageStream.START_OF_FRAME,
+        (byte) WatchMessageStream.lengthOnWire(empty),
+        0x42,
+        (byte) WatchMessageStream.computeCrc(empty),
+        (byte) (WatchMessageStream.computeCrc(empty) >> 8)
+    }, result);
+  }
+  
+  @Test public void testWriteNonEmpty() throws IOException {
+    WatchMessage msg = new WatchMessage((byte) 0x42,
+        new byte[] { 1, 2, 3, 4, 5 });
+    ByteArrayOutputStream sink = new ByteArrayOutputStream();
+    WatchMessageStream stream = new WatchMessageStream(sink);
+    
+    stream.write(msg);
+    
+    byte[] result = sink.toByteArray();
+    assertEquals(1 + 1 + 1 + 5 + 2, result.length);
+    assertArrayEquals(new byte[] {
+        WatchMessageStream.START_OF_FRAME,
+        (byte) WatchMessageStream.lengthOnWire(msg),
+        0x42,
+        1,2,3,4,5,
+        (byte) WatchMessageStream.computeCrc(msg),
+        (byte) (WatchMessageStream.computeCrc(msg) >> 8)
+    }, result);
   }
 }
